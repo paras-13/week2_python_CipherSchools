d733e326c3e4feb05491dbe0e39f70332b595d83
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-12 02:10:25 +0530

    Exercise 6 Number guessing game

--- Exercise6.py
+++ Exercise6.py
@@ -4,20 +4,18 @@ from random import randint as r
 r_num = r(0,100)
 flag = "down"
 times = 0
-u_num = int(input("Guess a number a between 0 1nd 100 : "))
+u_num = int(input("Guess a number between 0 and 100 : "))
 while flag == "down":
-    if u_num < r_num:
-        print("Too low")
-        times +=1
-        u_num = int(input("Guess again : "))
-        flag = "down"
-    elif u_num > r_num:
-        print("Too high")
-        u_num = int(input("Guess Again : "))
-        flag = "down"
-        times +=1
-    else:
+    if u_num == r_num:
         times +=1
         print(f"You win and you guessed this number in {times} times")
         flag = "fly"
-
+    else:
+        if u_num < r_num:
+            print("Too low")
+        else:
+            print("Too high")
+        times +=1
+        u_num = int(input("Guess Again : "))
+        flag = "down"
+        
