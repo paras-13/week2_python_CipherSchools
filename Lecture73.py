7a32455c44e86cdde2ee57196519264157866a7d
Paras Upadhyay <paras13upadhyay@gmail.com>
2022-12-12 03:52:36 +0530

    Lecture73 functions in python

---
+++ Lecture73.py
@@ -0,0 +1,13 @@
+# Functions
+
+# name="Paras"
+# print(len(name))
+
+def add_two(num1,num2):
+    return num1+num2
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+# total = add_two(a,b)
+# print(total)
+print(add_two(a,b))
